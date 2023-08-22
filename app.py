f80aa7e7433b80b25968473005d51d8bce6c8c46
gawad <gawad@unal.edu.co>
2023-08-22 17:54:17 -0500

    Update app.py
    Agregar pregunta sobre la ciudad.

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def main():
     if name:
         st.write(f"¡Hola, {name}! Te damos la bienvenida a tu app de saludos personalizados.")
 
+    ciudad = st.text_input("Por favor escriba el nombre de su lugar de nacimiento:")
+    if ciudad:
+        st.write(f"¡Hola, {name} de {ciudad}! Te damos la bienvenida a tu app de saludos personalizados.")
+
+
 if __name__ == "__main__":
     main()
