f4c3d26a9940c19172b0f9520061021687880524
gawad <gawad@unal.edu.co>
2023-08-22 19:59:19 -0500

    Update grafico.py
    Cambiando MatPlotLib por Plotly

--- grafico.py
+++ grafico.py
@@ -1,11 +1,20 @@
 import streamlit as st
-
-import matplotlib.pyplot as plt
-
 import numpy as np
+import plotly.figure_factory as ff
 
-arr = np.random.normal(1, 1, size=100)
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-plt.hist(arr, bins=20)
+# Group data together
+hist_data = [x1, x2, x3]
 
-st.pyplot()
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
